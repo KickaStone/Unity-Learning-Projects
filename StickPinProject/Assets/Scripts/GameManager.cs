73a9b9666cbb76a85f0adfeb02a14f0e5ef4a315
季俊丞 <callmejjc@gmail.com>
2019-09-22 13:07:01 +0800

    项目完成

--- StickPinProject/Assets/Scripts/GameManager.cs
+++ StickPinProject/Assets/Scripts/GameManager.cs
@@ -1,27 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     private Transform startPoint;
     private Transform spawnPoint;
     private Pin currentPin;
+    private int score = 0;//控制分数
+    public Text scoreText;//分数显示框
 
+    private Camera mainCamera;//主摄像机
+
+    private bool isGameOver = false;//确保GameOver只会被调用一次
     public GameObject pinPrefab;
+    public float speed = 3;//颜色变化速度
+
 	// Use this for initialization
 	void Start () {
+        //获取startPoint和spawnPoint的位置
         startPoint = GameObject.Find("StartPoint").transform;
         spawnPoint = GameObject.Find("SpawnPoint").transform;
-
-        SpawnPin();//调用方法
+        mainCamera = Camera.main;
+        SpawnPin();//生成针
 	    	
 	}
 
     private void Update() {
-        if(Input.GetMouseButtonDown(0))
+        if(isGameOver) return;
+        if(Input.GetMouseButtonDown(0))//鼠标左键点击事件响应
         {   
-            currentPin.StartFly();
+            score++;//分数增加
+            scoreText.text = score.ToString();//分数显示
+            currentPin.StartFly();//让针飞行
+            SpawnPin();//前一个针开始插的时候，生成新的针
         }
         
     }
@@ -31,6 +45,31 @@ public class GameManager : MonoBehaviour {
         currentPin = GameObject.Instantiate(pinPrefab, spawnPoint.position, pinPrefab.transform.rotation).GetComponent<Pin>();//生成gameobject 注意旋转使用Pin的旋转值
     }
 
+    public void GameOver(){
 
+        if(isGameOver)
+            return;
+        GameObject.Find("Circle").GetComponent<RotateSelf>().enabled = false;//让小球停止旋转
+        StartCoroutine(GameOverAnimation());
+
+        isGameOver = true;
+    }
+
+    IEnumerator GameOverAnimation()
+    {
+        while(true)
+        {
+            //颜色渐变（开始颜色 目标颜色 速度）
+            mainCamera.backgroundColor = Color.Lerp(mainCamera.backgroundColor, Color.red, speed * Time.deltaTime);
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 4, speed* Time.deltaTime);
+            if(Mathf.Abs(mainCamera.orthographicSize - 4) < 0.01f)
+            {
+                break;
+            }
+            yield return 0;
+        }
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex);//重新加载场景
+    }
 
 }
--- StickPinProject/Assets/Scripts/Pin.cs
+++ StickPinProject/Assets/Scripts/Pin.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class Pin : MonoBehaviour
 {
-
-    private float speed = 5;//speed
+    private float speed = 15;//speed
     private bool isFly = false; //to circle
     private bool isReach = false;//to startPoint
-    private Transform startPoint;
-	private Transform circle;
+    private Transform startPoint;//插针前针的位置
+    private Vector3 tragetCirclePos;//针实际插到的位置
+    private Transform circle;//Circle的位置
 
 
     // Use this for initialization
     void Start()
     {
+        //获取startPoint的位置
         startPoint = GameObject.Find("StartPoint").transform;
-		//circle = GameObject.FindGameObjectWithTag("Circle").transform;
+        //circle = GameObject.FindGameObjectWithTag("Circle").transform;
         circle = GameObject.Find("Circle").transform;
-
+        tragetCirclePos = circle.position;
+        tragetCirclePos.y -= 1.45f;//让针插到Circle的边上
     }
 
     // Update is called once per frame
@@ -30,21 +32,26 @@ public class Pin : MonoBehaviour
             {//创建后运动到startPoint
                 transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);//运动
                 if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)
-					isReach = true;//优化
+                    isReach = true;//优化
             }
-
-
         }
-		else
-		{
-			transform.position = Vector3.MoveTowards(transform.position, circle.position, speed * Time.deltaTime);//运动
-		}
+        else
+        {
+            //针插到Circle上的运动
+            transform.position = Vector3.MoveTowards(transform.position, tragetCirclePos, speed * Time.deltaTime);//运动
+            //距离判定 
+            if (Vector3.Distance(transform.position, tragetCirclePos) < 0.05f)
+            {
+                transform.position = tragetCirclePos;//为了对齐所有的针
+                transform.parent = circle;//设置变换父对象，让针和Circle一起旋转
+                isFly = false;//状态修改
+            }
+        }
     }
 
-	public void StartFly()
-	{
-		isFly = true;
-		isReach = true;
-
-	}
+    public void StartFly()//开始飞行
+    {
+        isFly = true;
+        isReach = true;
+    }
 }
